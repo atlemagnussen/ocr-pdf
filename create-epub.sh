8bb2d894fd225d40ab2d5540f250273e951ab6b0
Atle Magnussen <atlemagnussen@gmail.com>
2024-01-19 20:29:06 +0100

    ds

--- create-epub.sh
+++ create-epub.sh
@@ -1,9 +1,3 @@
-rm vesica-essential.epub
-
-# add mimetype 1st
-zip -0 -X vesica-essential.epub mimetype
-
-# add the rest
-zip -9 -X -r -u vesica-essential.epub *
-
-zip -d vesica-essential.epub create-epub.sh
+rm mman.epub
+zip -0 -X mman.epub mimetype
+zip -9 -X -r -u mman.epub *
